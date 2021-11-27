4b46446d96f394af58df7aac4588cd58cfd3210f
Zach Kelley <32417789+Roundhbout@users.noreply.github.com>
2021-11-27 15:24:20 -0500

    Day 7 part 2 done

--- Day7/day7.py
+++ Day7/day7.py
@@ -38,29 +38,57 @@ def parse_contains(contains):
 args: all_bags (parsed input in it's entirety), current_bag(str of current bag type)
 return number of shiny gold bags contained in current_bag
 """
-def target_bag_count(all_bags, current_bag):
+def target_bag_count(all_bags, current_bag, target_bag):
     count = 0
     bag_contents = all_bags[current_bag]
     if len(bag_contents) == 0:
         return count
     else:
         for sub_bag in bag_contents:
-            if sub_bag['bag_type'] == 'shiny gold':
+            if sub_bag['bag_type'] == target_bag:
                 count += 1
-            count += target_bag_count(all_bags, sub_bag['bag_type'])
+            count += target_bag_count(all_bags, sub_bag['bag_type'], target_bag)
     return count
 
 # returns number of bag colors that contain at least one shiny gold bag
 def bags_containing_shiny_gold(all_bags):
     count = 0
     for bag_name in all_bags.keys():
-        if target_bag_count(all_bags, bag_name):
+        if target_bag_count(all_bags, bag_name, 'shiny gold'):
             count += 1
     return count
+
+"""
+Return the number of bags contained within target bag
+"""
+def bags_inside_target(all_bags, target):
+    #init count, get current bag + it's content info from all_bags
+    count = 0
+    curr_bag = all_bags[target]
+    #if the bag is empty, return count (should be 0)
+    if len(curr_bag) == 0:
+        return count
+    # current bag contains at least one other type of bag of any given count
+    else:
+        # for ever sub bag in target
+        for bag in curr_bag:
+            # get the amount of sub bags and add it to the count
+            curr_bag_count = bag['num']
+            count += curr_bag_count
+            # get the amount of bags within a given sub bag, add that multiplied by the number of sub bags to count
+            bags_inside_curr_bag = bags_inside_target(all_bags, bag['bag_type'])
+            count += bags_inside_curr_bag * curr_bag_count
+
+    return count
+
 
 parsed_rules = {bag : parse_contains(contains) for bag, contains in rules.items()}
 
 def part_1():
     return bags_containing_shiny_gold(parsed_rules)
 
+def part_2():
+    return bags_inside_target(parsed_rules, 'shiny gold')
+
 print(f"PART 1: {part_1()}")
+print(f"PART 2: {part_2()}")
